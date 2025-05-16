ea0f16720205937f668d526769cc92995fb45f60
Kapi <kacperkozlowskii12@gmail.com>
2025-05-16 18:38:22 +0200

    Add Color Picker

--- script.js
+++ script.js
@@ -6,26 +6,43 @@ const body = document.querySelector("body")
 const buttonPrompt = document.createElement("button")
 buttonPrompt.textContent = 'Square Count'
 
-body.appendChild(buttonPrompt)
+const main = document.querySelector(".main")
+const sidebar = document.querySelector(".sidebar")
+const buttonColor = document.createElement("input")
+
+buttonColor.textContent = 'Select Color'
+buttonColor.setAttribute("type", "color")
+
+sidebar.appendChild(buttonPrompt)
+sidebar.appendChild(buttonColor)
+
+const squarediv = document.createElement("div")
 
 
 
 buttonPrompt.addEventListener("click", () => {
+    container.innerHTML = ''
+    
     let count = prompt("Square Count")
-    for(let i = 0; i < count; i++){
+    
+    const totalSquares = count * count
+    const squareSize = 400 / count
+
+  
+   
+    for(let i = 0; i < totalSquares; i++){
     const squarediv = document.createElement("div")
     squarediv.classList.add("squarediv")
+    squarediv.style.height = `${squareSize}px`
+    squarediv.style.width = `${squareSize}px`
     container.appendChild(squarediv)
-    squarediv.addEventListener("mouseover", () => squarediv.style ="background-color: blue")
-}
-
+    squarediv.addEventListener("mouseover", () => {
+    squarediv.style.backgroundColor = buttonColor.value
+})
+    }
 })
 
 
 
 
-
-function changeSquareAmount(count){
-
-}
 
