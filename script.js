2404bc47da0e1249823921f3724b0910786dbe23
Kapi <kacperkozlowskii12@gmail.com>
2025-05-17 12:15:30 +0200

    finish layout

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const body = document.querySelector("body")
 
 const buttonPrompt = document.createElement("button")
 buttonPrompt.textContent = 'Square Count'
+buttonPrompt.classList.add("count")
 
 const main = document.querySelector(".main")
 const sidebar = document.querySelector(".sidebar")
@@ -12,6 +13,7 @@ const buttonColor = document.createElement("input")
 
 buttonColor.textContent = 'Select Color'
 buttonColor.setAttribute("type", "color")
+buttonColor.classList.add("color")
 
 sidebar.appendChild(buttonPrompt)
 sidebar.appendChild(buttonColor)
@@ -23,26 +25,29 @@ const squarediv = document.createElement("div")
 buttonPrompt.addEventListener("click", () => {
     container.innerHTML = ''
     
+    
     let count = prompt("Square Count")
+    
+  
     
     const totalSquares = count * count
     const squareSize = 400 / count
 
   
+
    
+
     for(let i = 0; i < totalSquares; i++){
     const squarediv = document.createElement("div")
     squarediv.classList.add("squarediv")
     squarediv.style.height = `${squareSize}px`
     squarediv.style.width = `${squareSize}px`
     container.appendChild(squarediv)
+    
+    
     squarediv.addEventListener("mouseover", () => {
     squarediv.style.backgroundColor = buttonColor.value
+    {}
 })
     }
 })
-
-
-
-
-
